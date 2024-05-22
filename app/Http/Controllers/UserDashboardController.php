5d0be2066b40bdc97d49159dfd8edf2a4583021a
James Pigott <james.pigott@steadfastcollective.com>
2024-05-22 16:50:16 +0100

    Controller actions for events index, create, edit, store, update and delete routes

--- app/Http/Controllers/UserDashboardController.php
+++ app/Http/Controllers/UserDashboardController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers;
 
 use Statamic\View\View;
 use Statamic\Facades\User;
+use Illuminate\Support\Str;
+use Statamic\Facades\Asset;
+use Statamic\Facades\Entry;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\RedirectResponse;
+use App\Http\Requests\DeleteEventRequest;
+use App\Http\Requests\StoreEventRequest;
+use App\Http\Requests\UpdateEventRequest;
 use App\Http\Requests\UpdateUserDetailsRequest;
 use App\Http\Requests\UpdateUserOrganisationRequest;
+use Statamic\Modifiers\CoreModifiers;
 
 class UserDashboardController extends Controller
 {
@@ -85,5 +93,238 @@ class UserDashboardController extends Controller
         return redirect()->back()->with([
             'success' => 'Organisation details updated successfully'
         ]);
+    }
+
+    public function events(): View
+    {
+        $user = User::current();
+
+        $page = request()->input('page', 1);
+
+        $events = Entry::query()
+            ->where('collection', 'events')
+            ->where('created_by', $user->id)
+            ->orderBy('start_date', 'asc')
+            ->orderBy('start_time', 'asc')
+            ->paginate(10, 'page', $page);
+
+
+        return (new View)
+            ->layout('layout')
+            ->template('users.dashboard.my-events')
+            ->with([
+                'title' => "Dashboard",
+                'user' => $user,
+                'events' => $events
+            ]);
+    }
+
+    public function editEvent(string $entryId): View
+    {
+        $entry = $this->getEntry($entryId);
+        $categories = $entry->get('event_categories');
+        $eventOrganisers = $entry->get('organisers');
+
+        if(is_array($entry->content_area)) {
+            $coreModifiers = new CoreModifiers();
+            $eventContent = $coreModifiers->bardHtml($entry->content_area);
+
+        } else {
+            $eventContent = $entry->content_area;
+        }
+
+        return (new View)
+            ->layout('layout')
+            ->template('users.dashboard.my-events-edit')
+            ->with([
+                'title' => "Dashboard",
+                'entry' => $entry,
+                'categories' => $categories,
+                'event_organisers' => $eventOrganisers,
+                'event_content' => $eventContent
+            ]);
+    }
+
+    public function updateEvent(string $entryId, UpdateEventRequest $request): RedirectResponse
+    {
+        $entry = $this->getEntry($entryId);
+        $user = User::current();
+
+        $start = Carbon::parse($request->start);
+
+
+        $entry->slug(Str::slug(__(':name :start', [
+            'name' => $request->title,
+            'start' => $start->format('d-m-Y')
+        ])));
+
+        $entry->data([
+            'title' => $request->title,
+            'content' => $request->description,
+            'start_date' => $start,
+            'end_date' => Carbon::parse($request->end),
+            'free' => $request->free === 'on' ? true : false,
+            'virtual' => $request->virtual === 'on' ? true : false,
+            'attendance_information' => $request->attendance_information,
+            'accessibility_information' => $request->accessibility_information,
+            'latitude' => $request->lat,
+            'longitude' => $request->lng,
+            'address_line_1' => $request->address_line_1,
+            'address_line_2' => $request->address_line_2,
+            'town' => $request->town,
+            'postcode' => $request->postcode,
+            'content_area' => $request->content,
+            'booking_link' => $request->booking_link,
+            'cta' => $request->cta,
+            'cost_details' => $request->cost_details,
+            'created_by' => $user ? $user->id : null
+        ]);
+
+        if($request->file('image')) {
+
+            $file = $request->file('image');
+
+            $asset = Asset::make()->container('assets')->path(__('user-events/:name', [
+                'name' => $file->getClientOriginalName()
+            ]));
+
+            $asset->data([
+                'alt' => $request->title
+            ]);
+
+            $asset->upload($file);
+
+            $asset->save();
+
+            $entry->featured_image = $asset->path;
+        }
+
+        if($request->categories) {
+            $entry->event_categories = $request->categories;
+        }
+
+        if($request->organisers) {
+            $entry->organisers = $request->organisers;
+        }
+
+        $entry->save();
+
+        return redirect()->back()->with('success', 'Event details updated successfully');
+    }
+
+    public function deleteEvent(string $entryId, DeleteEventRequest $request): RedirectResponse
+    {
+
+        $entry = $this->getEntry($entryId);
+
+        if(!$entry) {
+            abort(404);
+        }
+
+        if($entry->featured_image) {
+            Asset::delete($entry->featured_image);
+        }
+
+        Entry::delete($entry);
+
+        return redirect(route('user.dashboard.my-events.index'))->with(
+            'success',
+            __("Event ':event' deleted successfully.", [
+                'event' => $entry->title
+            ])
+        );
+    }
+
+    public function createEvent(): View
+    {
+        return (new View)
+            ->layout('layout')
+            ->template('users.dashboard.my-events-create')
+            ->with([
+                'title' => "Dashboard",
+            ]);
+    }
+
+    public function storeEvent(StoreEventRequest $request): RedirectResponse
+    {
+        $user = User::current();
+
+        $start = Carbon::parse($request->start);
+
+        $entry = Entry::make()
+            ->collection('events')
+            ->published(false)
+            ->slug(Str::slug(__(':name :start', [
+                'name' => $request->title,
+                'start' => $start->format('d-m-Y')
+            ])))
+            ->data([
+                'title' => $request->title,
+                'content' => $request->description,
+                'start_date' => $start,
+                'end_date' => Carbon::parse($request->end),
+                'free' => $request->free === 'on' ? true : false,
+                'virtual' => $request->virtual === 'on' ? true : false,
+                'attendance_information' => $request->attendance_information,
+                'accessibility_information' => $request->accessibility_information,
+                'latitude' => $request->lat,
+                'longitude' => $request->lng,
+                'address_line_1' => $request->address_line_1,
+                'address_line_2' => $request->address_line_2,
+                'town' => $request->town,
+                'postcode' => $request->postcode,
+                'content_area' => $request->content,
+                'booking_link' => $request->booking_link,
+                'cta' => $request->cta,
+                'cost_details' => $request->cost_details,
+                'created_by' => $user ? $user->id : null
+            ]);
+
+        if($request->file('image')) {
+
+            $file = $request->file('image');
+
+            $asset = Asset::make()->container('assets')->path(__('user-events/:name', [
+                'name' => $file->getClientOriginalName()
+            ]));
+
+            $asset->data([
+                'alt' => $request->title
+            ]);
+
+            $asset->upload($file);
+
+            $asset->save();
+
+            $entry->featured_image = $asset->path;
+        }
+
+        if($request->categories) {
+            $entry->event_categories = $request->categories;
+        }
+
+        if($request->organisers) {
+            $entry->organisers = $request->organisers;
+        }
+
+        $entry->save();
+
+        return redirect(route('user.dashboard.my-events.index'))->with(
+            'success',
+            __("Event ':event' created successfully.", [
+                'event' => $entry->title
+            ])
+        );
+    }
+
+    private function getEntry(string $id)
+    {
+        $entry = Entry::find($id);
+
+        if(!$entry) {
+            abort(404);
+        }
+
+        return $entry;
     }
 }
