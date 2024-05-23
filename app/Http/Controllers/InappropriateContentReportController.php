a2a4e7ffb1f079bba1700952938e8d6b5dcf48be
James Pigott <james.pigott@steadfastcollective.com>
2024-05-23 09:52:07 +0100

    Dispatch content report job from content report controller

--- app/Http/Controllers/InappropriateContentReportController.php
+++ app/Http/Controllers/InappropriateContentReportController.php
@@ -5,17 +5,20 @@ namespace App\Http\Controllers;
 use App\Models\ContentReport;
 use Illuminate\Http\RedirectResponse;
 use App\Http\Requests\StoreContentReportRequest;
+use App\Jobs\NotifyEventApproversOfContentReport;
 
 class InappropriateContentReportController extends Controller
 {
     public function store(string $entryId, StoreContentReportRequest $request): RedirectResponse
     {
-        ContentReport::create([
+        $report = ContentReport::create([
             'entry_id' => $entryId,
             'reason' => $request->reason,
             'comments' => $request->comments,
             'email' => $request->email
         ]);
+
+        NotifyEventApproversOfContentReport::dispatch($report);
 
         return redirect()->back()->with([
             'openModal' => true,
