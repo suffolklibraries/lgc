3e081aa583ad51e4864365e7cecce121ad589f4d
André Breia <hey@andrebreia.dev>
2024-10-02 14:48:52 +0200

    Feature/events api (#15)
    * Add base API
    
    * Api authentication
    
    * Update IndexRequest.php
    
    * Update EventController.php
    
    * Update IndexRequest.php
    
    * Update web.php
    
    * Fix date searching
    
    ---------
    
    Co-authored-by: James Pigott <james.pigott@steadfastcollective.com>

---
+++ config/getcreative.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'events_api_key' => env('EVENTS_API_KEY')
+];
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\EventController;
+use App\Http\Middleware\TokenAuthentication;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware(TokenAuthentication::class)->group(function () {
+    Route::get('/events', [EventController::class, 'index']);
+});
